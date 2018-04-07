d93fa62dc7f50305dbff847417f58479aea7fb6b
Christopher Burkey <cburkey@openedit.org>
2018-04-06 20:21:09 -0400

    Data entry

--- www/components/teamt.js
+++ www/components/teamt.js
@@ -4,7 +4,22 @@ jQuery(document).ready(function()
 	jQuery.ajaxSetup({
 	    cache: false
 	});
-	console.log("Started");
+
+	$(".addnewassignment").click(function(e)
+	{
+		e.stopPropagation();
+		e.preventDefault();
+	
+		var div = $(this).closest(".rowdata");
+		var assign = $(".assignments",div);
+		var options = div.data();
+		var link = $(".schedule").data("addaction");
+		jQuery.get(link, options, function(data) 
+		{
+			assign.append(data);
+			$(".addnew input",div).focus();
+		});
+	});
 	
 });
 	
